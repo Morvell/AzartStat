15cf5a4f28fbeceae6ed863b103d78fe06049480
morvell <zaxarov2702@yandex.ru>
2017-03-12 15:26:28 +0500

    завершшен контракт

--- app/src/main/java/ru/azsoftware/azartstat/data/BetContract.java
+++ app/src/main/java/ru/azsoftware/azartstat/data/BetContract.java
@@ -10,16 +10,12 @@ public final class BetContract {
 
 
     public static final class BetEntry implements BaseColumns {
-        public final static String TABLE_NAME = "guests";
+        public final static String TABLE_NAME = "bet";
 
         public final static String _ID = BaseColumns._ID;
-        public final static String COLUMN_NAME = "name";
-        public final static String COLUMN_CITY = "city";
-        public final static String COLUMN_GENDER = "gender";
-        public final static String COLUMN_AGE = "age";
-
-        public static final int GENDER_FEMALE = 0;
-        public static final int GENDER_MALE = 1;
-        public static final int GENDER_UNKNOWN = 2;
+        public final static String COLUMN_DATE = "date";
+        public final static String COLUMN_BANK = "bank";
+        public final static String COLUMN_PROFIT = "profit";
+        
     }
 }
