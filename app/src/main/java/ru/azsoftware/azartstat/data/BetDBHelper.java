4024e331dc8cad4e4baedbe78d8e1af503def364
morvell <zaxarov2702@yandex.ru>
2017-03-14 22:12:04 +0500

    сделано добавление данных в БД

--- app/src/main/java/ru/azsoftware/azartstat/data/BetDBHelper.java
+++ app/src/main/java/ru/azsoftware/azartstat/data/BetDBHelper.java
@@ -24,7 +24,6 @@ public class BetDBHelper extends SQLiteOpenHelper {
         String SQL_CREATE_GUESTS_TABLE = "CREATE TABLE " + BetEntry.TABLE_NAME + " ("
                 + BetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + BetEntry.COLUMN_DATE + " TEXT NOT NULL, "
-                + BetEntry.COLUMN_BANK + " INTEGER NOT NULL DEFAULT 0, "
                 + BetEntry.COLUMN_PROFIT + " INTEGER NOT NULL DEFAULT 0);";
 
         // Запускаем создание таблицы
