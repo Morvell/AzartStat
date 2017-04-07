e407afbdf799a91a3319a91512cc0985d42de0de
morvell <zaxarov2702@yandex.ru>
2017-04-07 17:27:10 +0500

    preparing for release v0.1

--- app/src/main/java/ru/azsoftware/azartstat/fragment/MainFragment.java
+++ app/src/main/java/ru/azsoftware/azartstat/fragment/MainFragment.java
@@ -5,7 +5,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
